dc705eb0089ed95c8a2947865357f2b9334d688b
PedroDBautista <damianpb6271@yahoo.com>
2021-09-21 15:17:12 -0400

    Finished Basic AI moving to waypoints and looping around to them

--- GameboyGame/Assets/EnemyController.cs
+++ GameboyGame/Assets/EnemyController.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject waypoint;
-
-
-
+    // Waypoints for the Enemy Racers
+    [SerializeField] Transform[] waypoints;
+    // Movement Speed 
+    [SerializeField] float moveSpeed = 2f;
+    
+    private int waypointIndex;
+    private Rigidbody2D _rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       // _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        
+        if (waypointIndex <= waypoints.Length-1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, Time.deltaTime * moveSpeed);
+
+           // Vector2 newPosition = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, Time.deltaTime * moveSpeed);
+           //_rigidBody.MovePosition(transform.position * Time.deltaTime * moveSpeed);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+
+            if(waypointIndex == waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+        }
+
+    }
+
 }
