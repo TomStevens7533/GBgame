e73cefd25022ceaebee5857974958a09eb1ad40c
PedroDBautista <damianpb6271@yahoo.com>
2021-09-27 19:15:18 -0400

    Added Ai prototype prefab variant but not doing anything with it now

---
+++ GameboyGame/Assets/scripts/AIMovementController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovementController : MonoBehaviour
+{
+    public enum AIMode { followPlayer, followWaypoints };
+
+    [Header("AI settings")]
+    public AIMode aiMode;
+
+    Vector3 targetPosition = Vector3.zero;
+    Transform targetTransform = null;
+
+    MovementController movementController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementController = GetComponent<MovementController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        switch(aiMode)
+        {
+            case AIMode.followPlayer:
+                Fo
+        }
+        inputVector.x = 16.0f;
+        inputVector.y = 16.0f;
+
+        //movementController. = inputVector.y;
+        //movementController.SetInputVector(inputVector);
+    }
+
+    void FollowPlayer()
+    {
+        ;
+    }
+}
